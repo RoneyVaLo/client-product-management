3cdc72e132e66a95f8d68e61a15b6723004a5ff4
RoneyVaLo <108905119+RoneyVaLo@users.noreply.github.com>
2025-04-05 02:40:22 -0600

    create icons for the login and navigation bar

---
+++ src/components/icons/EyeOff.jsx
@@ -0,0 +1,20 @@
+const EyeOff = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="16"
+    height="16"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className="h-4 w-4 text-gray-500"
+    {...props}
+  >
+    <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
+    <circle cx="12" cy="12" r="3"></circle>
+  </svg>
+);
+
+export default EyeOff;
